03b096d6f7e166d597539a106e8e113e71bb2614
fasosnql <nowak_przemek@yahoo.com>
2020-07-25 16:50:04 +0200

    01-finish all files

--- front-app/01/src/App.js
+++ front-app/01/src/App.js
@@ -11,32 +11,36 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import {BookDetails} from "./components/BookDetails";
 import {BookAdd} from "./components/BookAdd";
+import { ApolloProvider } from '@apollo/client';
+import { client } from './graphql-services/client';
 
 function App() {
   return (
-    <Router>
-        <Navbar bg="light" expand="lg">
-            <Navbar.Brand>Book App</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-                <Nav.Link as={Link} to="/">Book list</Nav.Link>
-                <Nav.Link as={Link} to="/book/add">Add Book</Nav.Link>
-            </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-        <Switch>
-            <Route path="/book/details/:id">
-            <BookDetails />
-            </Route>
-            <Route path="/book/add">
-            <BookAdd />
-            </Route>
-            <Route path="/">
-            <BookList />
-            </Route>
-        </Switch>
-    </Router>
+    <ApolloProvider client={client}>
+        <Router>
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand>Book App</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link as={Link} to="/">Book list</Nav.Link>
+                    <Nav.Link as={Link} to="/book/add">Add Book</Nav.Link>
+                </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            <Switch>
+                <Route path="/book/details/:id">
+                <BookDetails />
+                </Route>
+                <Route path="/book/add">
+                <BookAdd />
+                </Route>
+                <Route path="/">
+                <BookList />
+                </Route>
+            </Switch>
+        </Router>
+    </ApolloProvider>
   );
 }
 
