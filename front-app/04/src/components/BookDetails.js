cbb687f9bdea0acf09bb3de8b992e7cd6909f557
fasosnql <nowak_przemek@yahoo.com>
2020-07-26 09:42:52 +0200

    04-finish all-files

--- front-app/04/src/components/BookDetails.js
+++ front-app/04/src/components/BookDetails.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {gql, useQuery} from '@apollo/client';
+import {gql, useQuery, useSubscription} from '@apollo/client';
 import {useParams} from 'react-router-dom';
 import {BookFragments} from '../graphql-services/book.fragments';
 
@@ -16,11 +16,38 @@ const BOOK_QUERY = gql`
     ${BookFragments.commonBook}
 `;
 
+const COMMENT_ADDED = gql`
+    subscription onCommentAdded($bookId: ID!) {
+        comments: commentAdded(bookId: $bookId) {
+            id
+            content
+        }
+    }
+`;
+
 export function BookDetails() {
+  const [comments, setComments] = useState([]);  
   const {id} = useParams();  
   const {data, loading, error} = useQuery(BOOK_QUERY, {
       variables: {
           id
+      },
+      onCompleted: ({ book }) => {
+          setComments(book.comments);
+      }
+  });
+  
+  useSubscription(COMMENT_ADDED, {
+      variables: {
+          bookId: id
+      },
+      onSubscriptionData: ({subscriptionData}) => {
+          const newComments = [
+              ...comments,
+              subscriptionData.data.comments
+          ];
+          
+          setComments(newComments);
       }
   });
   
@@ -33,8 +60,6 @@ export function BookDetails() {
   }
   
   const {book} = data;
-  
-  const {comments} = book;
 
   return (
     <div style={{margin: '1rem'}}>
